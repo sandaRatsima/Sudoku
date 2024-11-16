5769dbe3c2765334426b9396997ebdb0a8f91bae
Sanda Ratsima <sanda.ratsima@universite-paris-saclay.fr>
2024-11-16 01:58:49 +0100

    Generateur terminé

---
+++ Sudoku/src/SudokuGenerator.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+public class SudokuGenerator {
+	public static final int GRID_SIZE = 9;
+
+	public static final int EASY = 1;
+	public static final int MEDIUM = 2;
+	public static final int HARD = 3;
+	
+	public static int[][]sudokuGenerator(int difficulty) {
+		int[][] grid = {
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0, 0, 0 } 
+		};
+		int nbToBlank = 0;
+		switch(difficulty) {
+		case EASY :
+			nbToBlank = 45;
+			break;
+		case MEDIUM :
+			nbToBlank = 54;
+			break;
+		case HARD :
+			nbToBlank = 64;
+			break;
+		}
+		SudokuSolver.printBoard(grid);
+		Random rand = new Random();
+		for(int i = 0; i < GRID_SIZE; i++) {
+			int number = rand.nextInt(9)+1;
+			if(SudokuSolver.isValidPlacement(grid, number, i, 0)) {
+				grid[i][0] = number; 
+			}
+		}
+		SudokuSolver.solveBoard(grid);
+		for(int i = 0; i <nbToBlank; i++) {
+			int rowToBlank = rand.nextInt(9);
+			int colToBlank = rand.nextInt(9);
+			
+			grid[rowToBlank][colToBlank] = 0;
+		}
+		SudokuSolver.printBoard(grid);
+		
+		return grid;
+	}
+	
+	public static void main(String ... args) {
+		int [][] test = sudokuGenerator(3);
+	}
+}
