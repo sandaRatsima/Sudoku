25744385ead40a297da49d3da7c8ff0967f3de43
Sanda Ratsima <sanda.ratsima@universite-paris-saclay.fr>
2024-11-17 22:54:15 +0100

    Factorisation en une classe Sudoku

---
+++ Sudoku/src/Sudoku.java
@@ -0,0 +1,11 @@
+public class Sudoku {
+    private int[][] grid;
+    public Sudoku(int difficulty){
+        this.grid = SudokuGenerator.sudokuGenerator(difficulty);
+    }
+
+    public int[][] getGrid(){
+        return this.grid;
+    }
+
+}
